42f86ad04624c9fa4434915272c0c5ebc710e8c9
tolbinyork <simon.grundtman@gmail.com>
2025-12-09 20:53:36 +0100

    playwright

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,8 +26,7 @@ module.exports = {
       2
     ],
     'linebreak-style': [
-      'error',
-      'unix'
+       0
     ],
     'quotes': [
       'error',
---
+++ playwright.config.js
@@ -0,0 +1,19 @@
+/* eslint-env node */
+// playwright.config.js
+/** @type {import('@playwright/test').PlaywrightTestConfig} */
+const config = {
+  testDir: './e2e',          // only run tests from e2e folder
+  testIgnore: ['**/*.jest.*'], // ignore Jest tests
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:8081',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
+  use: {
+    baseURL: 'http://localhost:8081/',
+  },
+}
+
+
+module.exports = config
